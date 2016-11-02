cc5b63471c237b667fa68f2c43fff3c2fd3b0344
ouxuewen <ouxuewen@360.cn>
2016-11-02 17:36:24 +0800

    modify

--- static/js/section.js
+++ static/js/section.js
@@ -2,12 +2,24 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         verticalCentered: false,
         scrollOverflow: true,
+        afterLoad: function(anchorLink, index){
+            if(index == 2){
+                $("#section2 #slide1 .person1").addClass("animated slideInDown").show()
+                $("#section2 #slide1 .person2").addClass("animated slideInUp").show()
+                $("#section2 #slide1 .name").addClass("animated slideInUp").show()
+            }
+        },
+
         afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
             var loadedSlide = $(this);
 
             //first slide of the second section
             if (index == 2 && slideIndex == 1) {
-                alert("First slide loaded");
+                console.log("asdasdasdasd")
+                $("#section2 #slide2 .person1").addClass("animated slideInDown").show()
+                $("#section2 #slide2 .person2").addClass("animated slideInUp").show()
+                $("#section2 #slide2 .name").addClass("animated slideInUp").show()
+
             }
 
             //second slide of the second section (supposing #secondSlide is the
@@ -22,5 +34,5 @@ $(document).ready(function() {
         $(".hand").hide();
         $("#section1").append('<img class="animated bounceInDown board" src="static/img/section/section1/section1-board.png" />')
 
-    }, 3000)
+    }, 2000)
 })
